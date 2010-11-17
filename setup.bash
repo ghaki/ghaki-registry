c7a1f1096ddef646cfa57a5b0f38e75aab4fc4a0
Gerald Kalafut <gkalafut@ebay.com>
2010-11-17 00:10:17 -0800

    setup now using .setuprc

--- setup.bash
+++ setup.bash
@@ -1,14 +1,6 @@
-############################################################################
-export GAK_WORK_DIR=$(pwd)
-export GAK_WORK_SETUP=$(pwd)/../SETUP
-. ${GAK_WORK_SETUP}/startup.bash
-
-export GAK_WORK_IDEPS=( \
-  $(pwd)/../ghaki_common/lib \
+export GK_PROJECT_IDEPS=( \
+  "$(pwd)/../ghaki_common/lib" \
   )
-export GAK_WORK_GO_DIRS=( \
-  lib:${GAK_WORK_DIR}/lib/ghaki/registry \
+export GK_PROJECT_GO_DIRS=( \
+  "lib:${GK_PROJECT_DIR}/lib/ghaki/registry" \
 )
-
-. ${GAK_WORK_SETUP}/finish.bash
-############################################################################
